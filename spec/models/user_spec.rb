24650597871d5b2c16f53ca6b805ddf2f0e018e7
Jose Estrada <j.estrada.developer@gmail.com>
2021-10-07 16:23:03 -0500

    Add user role field to users table We need to add a field to implement the authorization functionality and avoid non admin users can regiter or create new users.
    Co-authored-by: CUBOFIG <hfigueroa@ucol.mx>
    Co-authored-by: Juanequex <ramon.g.uz37@gmail.com>
    Co-authored-by: adrianMRJ <adrian.mrj.97@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do 
+    @user_without_email = User.new(email: "example1@test.com")
+    @user_without_strong_password = User.new(email: "example2@test.com", password: "12345")
+    @user_strong_password = User.new(email: "example2@test.com", password: "123456")
+    @user_super_admin = User.create!(email: "example3@test.com", password: "123456", superadmin_role: true)
+    @user_no_user_role = User.create!(email: "example4@test.com", password: "123456", superadmin_role: true, user_role: false)
+  end
+  it "has a password empty field" do
+    expect(@user_without_email.save).to be_falsey 
+  end
+  it "has a password with less of 6 characters" do
+    expect(@user_without_strong_password.save).to be_falsey 
+  end
+  it "has a password with 6 characters" do
+    expect(@user_strong_password.save).to be_truthy 
+  end
+  it "has a true user role field by default" do
+    expect(@user_strong_password.user_role).to be_truthy 
+  end
+  it "has a true superadmin user role field" do
+    expect(@user_super_admin.superadmin_role).to be_truthy
+  end
+  it "has a true superadmin user role field and false user role field" do
+    expect(@user_no_user_role.user_role).to be_falsey
+  end
+  
 end
